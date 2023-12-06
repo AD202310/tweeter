041b544bd427817d4f3c4d112feb3b64f570f1b9
AD202310 <a.g.dychenko@gmail.com>
2023-12-06 16:30:33 -0500

    Rearrange client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (data) => {
-  const newTweet = `
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const newTweet = createTweetElement(tweet);
+    $( '#tweets-container' ).append(newTweet);
+  }
+};
+
+const createTweetElement = function(data) {
+  let $tweet = `
     <article class="tweet">
       <header class="flex-container-row">
         <div class="profile-pic-username flex-container-row">
@@ -27,28 +60,9 @@ const createTweetElement = (data) => {
       </footer>
     </article>
     `;
-  return newTweet;
-
+  return $tweet;
 };
 
-const renderTweets = (data) => {
-  for (const tweet of data) {
-    const newTweet = createTweetElement(tweet);
-    $( '#tweets-container' ).append(newTweet);
-  }
-};
-
-const data = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-};
 
 $( document ).ready(function() {
 renderTweets(data);
