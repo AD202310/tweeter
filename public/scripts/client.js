946c287e2d5f38be02bf5b2db1bcad03719db55e
AD202310 <a.g.dychenko@gmail.com>
2023-12-06 20:24:11 -0500

    Implement loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,39 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    const newTweet = createTweetElement(tweet);
-    $( '#tweets-container' ).append(newTweet);
-  }
-};
-
 const createTweetElement = function(data) {
   let $tweet = `
     <article class="tweet">
@@ -63,12 +30,25 @@ const createTweetElement = function(data) {
   return $tweet;
 };
 
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const newTweet = createTweetElement(tweet);
+    $( '#tweets-container' ).append(newTweet);
+  }
+};
+
+const loadTweets = () => {
+  $.get('/tweets').then((data) => {
+    renderTweets(data);
+  });
+};
 
 $( document ).ready(function() {
-renderTweets(data);
 $( '.new-tweet form' ).submit(function(event) {
   event.preventDefault();
   const tweetData = $( this ).serialize();
-  $.post('/tweets/', tweetData);
+  $.post('/tweets/', tweetData).then( function() {
+    loadTweets();
+  })
 });
 });
